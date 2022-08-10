d4470478e6b76e23e3c64b465f7301be68af7ac5
Chingiskhan Nugrazin <110384143+ChingiskhanBaltaKerey@users.noreply.github.com>
2022-08-10 13:17:11 +0600

    Task #1

---
+++ ex02/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine ("The program written by Chingiskhan");
+int a = 5;
+int b = 7;
+int max = b;
+int min = a;
+
+if (a > b) a = max;
+if (b > a) b = max;
+Console.Write ("max = ");
+Console.WriteLine (max);
+Console.Write ("min = ");
+Console.WriteLine (min);
