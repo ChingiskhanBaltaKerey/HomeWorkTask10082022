8333934ff537a5a4f70bb05827222d857fdc803c
Chingiskhan Nugrazin <110384143+ChingiskhanBaltaKerey@users.noreply.github.com>
2022-08-10 14:31:56 +0600

    ex#4

---
+++ ex04/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine ("Ex04 Written by Chingiskhan");
+int a = 2;
+int b = 3; 
+int c = 7;
+int max = a;
+if (b > max) b = max;
+if (c > b) c = max;
+Console.Write ("max = ");
+Console.WriteLine (max); 
+
+
